f6a3a336e5ba299777955d48c670d26c8b51523b
jan <janskaaden@gmail.com>
2019-03-29 11:55:20 +0100

    Table

---
+++ JS/payment.js
@@ -0,0 +1,11 @@
+var PurchaseBtn = document.getElementById("proceedpurchase")
+
+PurchaseBtn.onclick= function(){
+    
+    console.log('clicked')
+}
+
+Add logoutbutton
+Add a table with searchfilter
+Check who the person logged in is
+Property savedevents
