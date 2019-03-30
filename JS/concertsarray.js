efa136282249f6fab7f46eb9b3323d61dae19802
deboraceccato <deboraceccato@outlook.it>
2019-03-30 09:30:46 +0100

    Filters

--- JS/concertsarray.js
+++ JS/concertsarray.js
@@ -1,86 +1,71 @@
-/* class Concerts{ // the class is like an object creation factory
-    constructor(concert, artist, genre, location, dateConcert) { // keys of a class
-        this.title = title;
-        this.artist = artist;
-        this.genre = genre;
-        this.location = location;
-        this.date = dateConcert;
-    }
-}
-
-var concerts = [{
-    title: "Sweetener European Tour",
-    artist: "Ariana Grande",
-    genre: "Pop",
-    location:"Paris",
-    date: new Date(2019, 5, 12),
-    price: "", 
-}, {
-    title: "Injured Generation Tour",
-    artist: "A$AP Rocky",
-    genre: "Hip-Hop / Rap",
-    location:"Stockholm",
-    date: new Date(2019, 8, 17),
-}, {
-    title: "#billylockettEUtour",
-    artist: "Billy Lockett",
-    genre: "Pop",
-    location:"Copenhagen",
-    date: new Date(2019, 2, 31),
-}, {
-    title: "Reading Festival",
-    artist: "Blossoms",
-    genre: "Alternative",
-    location:"Reading",
-    date: new Date(2019, 10, 25),
-}, {
-    title: "Sweetener European Tour",
-    artist: "Calcutta",
-    genre: "Alternative",
-    location:"Milan",
-    date: new Date(2019, 7, 3),
-}]
-
-
 const searchBtn = document.getElementById("searchbtn");
 const artistTag = document.getElementById("dropDown1");
 const genreTag = document.getElementById("dropDown2");
 const locationTag = document.getElementById("dropDown3");
 
 // create a variable filters that allows us to filter the events according to artist, genre, location and date - use empty strings as default value 
+// object with different properties (same as the ones specified in the class of concerts)
 var filters = {
     artist: '',
     genre: '',
     location: '',
-    date: ''
+    date: '',
+    searchText: ''
 }
 
-
+// creating a filtered array based on the user selection
+// this is a function with two parameters (array of concerts and filters)
 const filterEvents = function(arrayOfConcerts, filter) {
-
-    const filteredConcerts = arrayOfConcerts.filter(function(concert) {
-        
+    const filteredConcerts = arrayOfConcerts.filter(function(concert) { // this is callback function (function within a function) that filters everything - 
+        // almost like a for loop, but here there is no need to put indeces
+        // callbackfunction has one argument and we use the singular (concert instead of concerts because the function will consider one concert at a time)
+        // returns the concerts according to the filter criteria - users need to select something or even leave the default options (includes method)
+        // we want to access the property of artist of the concert - includes method means that concert.artist property matches the filter.artist
+        // includes method determines whether one string may be found within another string
         return concert.artist.includes(filter.artist) && concert.genre.includes(filter.genre) && concert.location.includes(filter.location)
     })
-
-    return filteredConcerts
+    // searches through all of the concerts in the array and uses filters
+    // this shows the events according to the criteria selected
+    // the callback function returns the function itself
+     return filteredConcerts
 }
 
-
-
-searchBtn.onclick = function () {
-    console.log(artistTag.value + genreTag.value + locationTag.value)
+// searchBtn.onclick
+artistTag.onchange = function () {
+    // Read out the search criteria & update our filters object
+    // the function recognised which option has been selected by the user for each filter
+    
+    // access the filter properties (artist, genre and location) and put them equal to the value in the tag
     filters.artist = artistTag.value
     filters.genre = genreTag.value
     filters.location = locationTag.value
 
+    // we use the filterEvents function to create a filtered array with only
+    // the concerts that match the search values
+    // the parameters of the filterEvents function are the whole array of concerts (var) and the filters (var)
+    const filteredConcerts = filterEvents(concerts, filters)
 
-    const listOfConcerts = filterEvents(concerts, filters)
-
-    console.log(listOfConcerts)
-
-   //var tag1 = artistTag1[i].value;
-   //var tag2 = genreTag2[i].value;
-   //var tag3 = document.getElementsByTagName("option")[i].value; 
+    // we call the renderHTML() to create the HTML code for every concert
+    // in the array
+    renderHTML(filteredConcerts)
 }
-*/
+
+
+// the parameter could be called anytype - but needs to be specified
+function renderHTML(list) {
+    var html = ''
+// we introduce the var html='' empty to make sure that the function erases the whole list of concerts available everytime the user launches a search
+// and shows only the concerts that successfully matches the search criteria
+    for(let i = 0; i < list.length; i++) {
+        html += list[i].renderConcert()
+    }
+    // this allows to show the specific concert
+    // "events" refers to the id in the table in html
+    document.getElementById('events').innerHTML = html
+}
+
+// prints the concert/s that successfully match the user's search
+renderHTML(concerts)
+
+// this shows the final results of the search - the search can restart many times - try to link to the buy tickets button
+// search on stack overflow for some help
--- JS/concertsclass.js
+++ JS/concertsclass.js
@@ -1,22 +1,32 @@
-class Concerts{ // the class is like an object creation factory
-    constructor(title, artist, genre, location, dateConcert, price) { // keys of a class
-        this.title = title;
+class Concert{ // the class is like an object creation factory
+    constructor(artist, genre, location, dateConcert, price) { // keys of a class
         this.artist = artist;
         this.genre = genre;
         this.location = location;
         this.date = dateConcert;
-        this.price = price
+        this.price = price;
+        // this.img = img;
+        // this.id = id;
+        // add the concert id in order to allow payments for later use
+    }
+    
+    // this function (which could be called in any way) allows to show the "table" of concerts and we put the html commands on the js itself
+    renderConcert() {
+        var img = "https://media4.s-nbcnews.com/j/newscms/2019_07/2746596/190208-ariana-grande-mc-1535_163b7df03fbfa03ba35bc8828f9eb918.fit-760w.JPG"
+        return '<tr style="background:white"><td><img src="' + img + '" style="height:250px"></td><td><h1>'+ this.artist +'</h1><p>Location:'+this.location+'</p><p>Price:'+this.price+'</p></td><td><button id='+this.id+'>Buy tickets</button></td></tr>'
     }
 }
 
-var concerts = []
-concerts.push = new Concert(title, artist, genre, location, dateConcert, price)
 
-new Concert("Ariana Grande", "Pop", "Paris", new Date(2019, 5, 12), "50")
-new Concert("A$AP Rocky", "Hip-Hop / Rap", "Stockholm", new Date(2019, 8, 17), "48")
-new Concert("Billy Lockett", "Pop", "Copenhagen", new Date(2019, 2, 31),"32")
-new Concert("Blossoms", "Alternative", "Reading", new Date(2019, 10, 25), "52")
-new Concert("Calcutta", "Alternative", "Milan", new Date(2019, 7, 3), "18")
-new Concert("Dua Lipa", "Pop", "Berlin", new Date(2019, 6, 22), "47")
-new Concert("Kasabian", "Rock", "Manchester", new Date(2019, 11, 2), "27")
-new Concert("")
+// add the url source for the image for each concert event so that each one show a different picture
+var concerts = [new Concert("Ariana Grande", "Pop", "Paris", new Date(2019, 5, 12), "50"),
+                new Concert("A$AP Rocky", "Hip-Hop / Rap", "Stockholm", new Date(2019, 8, 17), "48"),
+                new Concert("Billy Lockett", "Pop", "Copenhagen", new Date(2019, 2, 31),"32"),
+                new Concert("Blossoms", "Alternative", "Reading", new Date(2019, 10, 25), "52"),
+                new Concert("Calcutta", "Alternative", "Milan", new Date(2019, 7, 3), "18"),
+                new Concert("Dua Lipa", "Pop", "Berlin", new Date(2019, 6, 22), "47"),
+                new Concert("Kasabian", "Rock", "Manchester", new Date(2019, 11, 2), "27"),
+                new Concert("Alvaro Soler", "Latin", "Madrid", new Date(2019, 9, 14), "33"),
+                new Concert("Dolly Parton", "Country", "Los Angeles", new Date(2019, 4, 11), "29"),
+                new Concert("Still Searching", "Jazz", "Zurich", new Date(2019, 2, 11), "13")]
+
